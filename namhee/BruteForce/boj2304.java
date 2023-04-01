8a94cd1165f23d1be6e7915095db757604290492
Namhee Kim <49919262+nhee-dev@users.noreply.github.com>
2023-04-02 01:40:59 +0900

    [No.40] BOJ2304 - 창고 다각형 / 실버2 / 브루트포스 (Update)
    - 시간 : 152ms -> 132ms
    - 정렬을 없앤 게 큰 듯.

--- namhee/BruteForce/boj2304.java
+++ namhee/BruteForce/boj2304.java
@@ -5,11 +5,12 @@ import java.util.StringTokenizer;
 
 public class Main {
     static int N;
-    static int[] L;
-    static int[] H;
-    static int midL, leftL, rightL;
+    static int[] arr = new int[1001];
+    static int start = 1001;
+    static int end = 0;
+    static int midL, maxH = 0;
     static int answer;
-    
+
     public static void main(String args[]) throws IOException {
         input();
         solve();
@@ -20,72 +21,52 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         
-        L = new int[N];
-        H = new int[N];
+        int L, H;
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            L[i] = Integer.parseInt(st.nextToken());
-            H[i] = Integer.parseInt(st.nextToken());
+            L = Integer.parseInt(st.nextToken());
+            H = Integer.parseInt(st.nextToken());
+            arr[L] = H;
+            
+            initPointLocation(L, H);
+        }
+    }
+    
+    static void initPointLocation(int L, int H) {
+        start = L < start ? L : start;
+        end = L > end ? L : end;
+        
+        if (maxH < H) {
+            maxH = H;
+            midL = L;
         }
     }
     
     static void solve() {
-        sortDescendingByH();
-        getArea();
-    }
-    
-    static void sortDescendingByH() {
-        for (int i = 0; i < N - 1; i++) {
-            for (int j = i + 1; j < N; j++) {
-                if (H[i] < H[j]) {
-                    swapHL(i, j);
-                }
-            }
-        }
-    }
-    
-    static void swapHL(int i, int j) {
-        int tmpH, tmpL;
-        tmpH = H[i];
-        H[i] = H[j];
-        H[j] = tmpH;
-        
-        tmpL = L[i];        
-        L[i] = L[j];
-        L[j] = tmpL;
-    }
-    
-    static void getArea() {
-        getHighestArea();
         getLeftArea();
         getRightArea();
     }
     
-    static void getHighestArea() {
-        answer += H[0];
-        midL = L[0];
-    }
-    
     static void getLeftArea() {
-        leftL = midL;
-        for (int i = 1; i < N; i++) {
-            if (midL > L[i] && leftL > L[i]) {
-                answer += ((leftL - L[i]) * H[i]);
-                leftL = L[i];
+        int h = 0;
+        for (int i = start; i <= midL; i++) {
+            if (h < arr[i]) {
+                h = arr[i];
             }
+            answer += h;
         }
     }
-    
+
     static void getRightArea() {
-        rightL = midL;
-        for (int i = 1; i < N; i++) {
-            if (midL < L[i] && rightL < L[i]) {
-                answer += ((L[i] - rightL) * H[i]);
-                rightL = L[i];
+        int h = 0;
+        for (int i = end; i > midL; i--) {
+            if (h < arr[i]) {
+                h = arr[i];
             }
+            answer += h;
         }
     }
-    
+
     static void output() {
         System.out.println(answer);
     }
