0d05d10eb6a8a1695e7b697c496dc56fe5ce9092
Namhee Kim <49919262+nhee-dev@users.noreply.github.com>
2023-04-04 19:41:11 +0900

    [No.41] PGS - 최소직사각형 / level1 / 완전탐색
    https://school.programmers.co.kr/learn/courses/30/lessons/86491
    
    다른 사람 풀이 보고 느낀 점.
     - Math.max(), min()으로 불필요한 분기를 줄이고
     - 중복되는 문장을 줄이는 게 인상 깊었다.

---
+++ namhee/BruteForce/pgs최소직사각형.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int solution(int[][] sizes) {
+        int max = 0, maxOfMin = 0;
+        for (int[] size : sizes) {
+			      max = Math.max(max, Math.max(size[0], size[1]));
+            maxOfMin = Math.max(maxOfMin, Math.min(size[0], size[1]));
+        }        
+        
+        int answer = max * maxOfMin;
+        return answer;
+    }
+}
+
+// class Solution {
+    
+//     public int solution(int[][] sizes) {
+//         int max = getMax(sizes);
+//         int maxOfMin = getMaxOfMin(sizes);
+        
+//         int answer = max * maxOfMin;
+//         return answer;
+//     }
+    
+//     int getMax(int[][] sizes) {
+//         int max = 0;
+//         for (int i = 0; i < sizes.length; i++) {
+//             max = sizes[i][0] > max ? sizes[i][0] : max;
+//             max = sizes[i][1] > max ? sizes[i][1] : max;
+//         }
+        
+//         return max;
+//     }
+    
+//     int getMaxOfMin(int[][] sizes) {
+//         int maxOfMin = 0;
+//         for (int i = 0; i < sizes.length; i++) {
+//             if (sizes[i][0] < sizes[i][1]) {
+//                 maxOfMin = sizes[i][0] > maxOfMin ? sizes[i][0] : maxOfMin;
+//             }
+//             else {
+//                 maxOfMin = sizes[i][1] > maxOfMin ? sizes[i][1] : maxOfMin;
+//             }
+//         }
+        
+//         return maxOfMin;
+//     }
+}
