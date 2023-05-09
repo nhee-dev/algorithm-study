9c107a4c5bd722ed6aacfc835787bb54bb12fc26
SANG\jdsae <jdsaeyqi@naver.com>
2023-05-09 21:02:56 +0900

    [No.150] pg121684 / 체육대회 / pccp모의고사1 2번 / dfs

--- sangwook/dfs/pg121684.java
+++ sangwook/dfs/pg121684.java
@@ -9,6 +9,7 @@ public class pg121684 {
         int sportsNum = ability[0].length;
         boolean[] visit = new boolean[studentNum];
         dfs(0,0,ability,visit);
+
         return max;
     }
 
