dfbcd470e1a5c2896ad56d90e99b9ee16f7d576c
Namhee Kim <49919262+nhee-dev@users.noreply.github.com>
2023-06-14 22:44:07 +0900

    [No.58] PGS - 짝지어 제거하기 / level2 / 스택
    https://school.programmers.co.kr/learn/courses/30/lessons/12973
    
    풀이 방법 : char[] 배열을 stack처럼 사용해 풀이

---
+++ namhee/data-structure/stack-queue/pgs_짝지어_제거하기/Solution1.java
@@ -0,0 +1,21 @@
+// Stack을 사용 (stack 구현을 배열로 해 봄.)
+class Solution {
+    public int solution(String s) {
+        int len = s.length();
+        
+        char ch;
+        int top = -1;
+        char[] stack = new char[len];
+        for (int i = 0; i < len; i++) {
+            ch = s.charAt(i);
+            if (top >= 0 && stack[top] == ch) {
+                top--; // pop
+            }
+            else {
+                stack[++top] = ch; // push
+            }
+        }
+        
+        return top == -1 ? 1 : 0;
+    }
+}
